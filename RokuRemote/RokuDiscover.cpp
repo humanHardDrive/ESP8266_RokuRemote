9323f67b3bb74c5bae62bbb526b681cbeda03068
Sam Stratter <humanHardDrive@gmail.com>
2020-01-14 22:00:08 -0500

    Setup discovery methods

--- RokuRemote/RokuDiscover.cpp
+++ RokuRemote/RokuDiscover.cpp
@@ -1,1 +1,47 @@
 #include "RokuDiscover.h"
+
+const char* DISCOVERY_REQUEST = "M-SEARCH * HTTP/1.1\r\n"
+                                "Host: 239.255.255.250:1900\r\n"
+                                "Man: \"ssdp:discover\"\r\n"
+                                "ST: roku:ecp\r\n\r\n";
+
+RokuDiscover::RokuDiscover() :
+  m_bInDiscovery(false),
+  m_nDiscoveredRoku(0)
+{
+
+}
+
+void RokuDiscover::update()
+{
+  if (m_bInDiscovery)
+  {
+    doDiscovery();
+
+    if ((millis() - m_DiscoveryStarted) > m_DiscoveryTimeout)
+      m_bInDiscovery = false;
+  }
+}
+
+void RokuDiscover::startDiscovery(uint32_t timeout)
+{
+  m_nDiscoveredRoku = 0;
+  m_DiscoveryTimeout = timeout;
+
+  m_UDPClient.beginPacket(IPAddress(239, 255, 255, 250), 1900);
+  m_UDPClient.write(DISCOVERY_REQUEST);
+  m_UDPClient.endPacket();
+
+  m_DiscoveryStarted = millis();
+  m_bInDiscovery = true;
+}
+
+void RokuDiscover::stopDiscovery()
+{
+  m_bInDiscovery = false;
+}
+
+void RokuDiscover::doDiscovery()
+{
+
+}
--- RokuRemote/RokuDiscover.h
+++ RokuRemote/RokuDiscover.h
@@ -5,11 +5,37 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
+#define MAX_ROKU_CLIENTS  16
+
+struct RokuInfo
+{
+  IPAddress address;
+  uint16_t port;
+};
+
 class RokuDiscover
 {
   public:
+    RokuDiscover();
+
+    void update();
+
+    void startDiscovery(uint32_t timeout);
+    void stopDiscovery();
+    void doDiscovery();
+
+    uint8_t getNumDiscovered() {
+      return m_nDiscoveredRoku;
+    }
 
   private:
+    WiFiUDP m_UDPClient;
+
+    bool m_bInDiscovery;
+    uint32_t m_DiscoveryTimeout, m_DiscoveryStarted;
+
+    uint8_t m_nDiscoveredRoku;
+    RokuInfo m_DiscoveredRoku[MAX_ROKU_CLIENTS];
 };
 
 #endif
